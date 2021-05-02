6338acd05c85d17ad2ad784fe986c975e29f9fd3
Weston Partin <djwest91@gmail.com>
2021-05-02 07:27:27 -0500

    import & add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "../src/components/ui/Header";
 
 function App() {
-  return <div className="container">Hello</div>;
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
